d0f8deb5a6fcc88b2f90f9a86ca69f0d275ddf2c
Aimesavage <Aimewestone@gmail.com>
2025-02-07 23:15:26 -0500

    Working version before HTML parsing

--- browser.py
+++ browser.py
@@ -24,6 +24,18 @@ class Tag:
         self.tag = tag
 
 
+FONTS = {}
+
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
+
 class Layout:
 
     def __init__(self, tokens):
@@ -33,9 +45,11 @@ class Layout:
         self.weight = "normal"
         self.style = "roman"
         self.size = 12
+        self.line = []
 
         for tok in tokens:
             self.token(tok)
+        self.flush()
 
     def token(self, tok):
         if isinstance(tok, Text):
@@ -57,20 +71,39 @@ class Layout:
             self.size += 4
         elif tok.tag == "/big":
             self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
 
     def word(self, word):
-        font = tkinter.font.Font(
-            size=16,
-            weight=self.weight,
-            slant=self.style
-        )
+        font = get_font(self.size, self.weight, self.style)
         w = font.measure(word)
-        self.display_list.append(
-            (self.cursor_x,  self.cursor_y, word, font))
+        self.line.append(
+            (self.cursor_x, word, font))
         self.cursor_x += w + font.measure(" ")
-        if self.cursor_x + w >= WIDTH - HSTEP:
-            self.cursor_y += font.metrics("linespace") * 1.25
-            self.cursor_x = HSTEP
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+            # self.cursor_y += font.metrics("linespace") * 1.25
+            # self.cursor_x = HSTEP
+
+    def flush(self):
+        if not self.line:
+            return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+
+        max_descent = max([metric["descent"] for metric in metrics])
+
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = HSTEP
+        self.line = []
 
 
 class Browser:
@@ -183,6 +216,7 @@ def lex(body):
         elif c == ">":
             in_tag = False
             out.append(Tag(buffer))
+            buffer = ""
         else:
             buffer += c
     if not in_tag and buffer:
